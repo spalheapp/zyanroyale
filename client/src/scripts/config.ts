5a107c3a99f0ab36515e01f9be9c427ca520df09
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-03-09 09:37:29 -0300

    Update config.ts

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,10 +1,6 @@
 export const Config = {
     regions: {
-        dev: { name: "Batalha Solo", address: "127.0.0.1:8000", https: true },
-        na: { name: "North America", address: "na.suroi.io", https: true },
-        eu: { name: "Europe", address: "eu.suroi.io", https: true },
-        sa: { name: "South America", address: "sa.suroi.io", https: true },
-        as: { name: "Asia", address: "as.suroi.io", https: true }
+        dev: { name: "Batalha Solo", address: "127.0.0.1:8000", https: false },
     },
     defaultRegion: "na",
     mode: "normal"

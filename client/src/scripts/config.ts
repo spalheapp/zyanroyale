31a0ac491285421905e0b87190aa21bdfa9cc03d
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-03-09 13:26:09 -0300

    Update config.ts

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,8 +1,6 @@
 export const Config = {
     regions: {
-        dev: { name: "Mata Mata", address: "146.19.9.234", https: true },
-        no: { name: "Campeonato", address: "162.159.24.201", https: true },
-        pt: { name: "Vai com tudo", address: "31.170.166.231", https: true }
+        dev: { name: "Mata Mata", address: "146.19.9.234:8000", https: true },
     },
     defaultRegion: "dev",
     mode: "normal"

b89e6ee7265facc616646b8a4b40752150e33bcf
Matheus Lúcio <matheusluciox@gmail.com>
2024-03-09 20:23:42 -0300

    Fix IP

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,17 +1,20 @@
 export const Config = {
-    regions: {
-        dev: { name: "Mata Mata", address: "146.19.9.234:8000", https: true },
-    },
-    defaultRegion: "dev",
-    mode: "normal"
+  regions: {
+    dev: { name: "Mata Mata", address: "146.19.9.234", https: true },
+  },
+  defaultRegion: "dev",
+  mode: "normal",
 } satisfies ConfigType as ConfigType;
 
 export interface ConfigType {
-    readonly regions: Record<string, {
-        readonly name: string
-        readonly address: string
-        readonly https: boolean
-    }>
-    readonly defaultRegion: string
-    readonly mode: string
+  readonly regions: Record<
+    string,
+    {
+      readonly name: string;
+      readonly address: string;
+      readonly https: boolean;
+    }
+  >;
+  readonly defaultRegion: string;
+  readonly mode: string;
 }

1d80f645837ea68a79a2491893c48de13570b425
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-03-09 00:51:08 -0300

    Update rulePosts.ts

--- client/src/scripts/news/rulePosts.ts
+++ client/src/scripts/news/rulePosts.ts
@@ -11,47 +11,47 @@ export const rules: RulesPost[] = [
     {
         date: 1690908230000,
         author: "Katloo",
-        title: "Rules",
-        content: `By playing Suroi, you agree to abide by the following rules. Breaking the rules may result in a ban.`,
+        title: "Regras",
+        content: `Ao jogar Zyan Royale, você concorda em cumprir as seguintes regras. Quebrar as regras pode resultar em banimento.`,
         longContent: `<ul>
-  <li><strong>No teaming in solo.</strong> Allying with other solo players for extended periods of time is not allowed.</li>
-  <li><strong>No hacking.</strong> The use of scripts, plugins, extensions, etc. to modify the game in order to gain an advantage over opponents is strictly forbidden.</li>
-  <li><strong>No inappropriate usernames.</strong> Our blocklists are very lenient, but some things are still blocked. Trying to evade filters for these specific words may incur consequences.</li>
+  <li><strong>Nenhuma equipe solo.</strong> Não é permitido aliar-se a outros jogadores solo por longos períodos de tempo.</li>
+  <li><strong>Sem hackers.</strong> É estritamente proibido o uso de scripts, plugins, extensões, etc. para modificar o jogo a fim de obter vantagem sobre os adversários.</li>
+  <li><strong>Nenhum nome de usuário inapropriado.</strong> Nossas listas de bloqueio são muito brandas, mas algumas coisas ainda estão bloqueadas. Tentar escapar dos filtros para essas palavras específicas pode ter consequências.</li>
 </ul>`
     },
     {
         date: 1690908230000,
         author: "Katloo",
         title: "Tutorial",
-        content: `If you're a new player, check out this tutorial to learn how to play!`,
+        content: `Se você é um jogador novo, confira este tutorial para aprender como jogar!`,
         longContent: `<h2>Controls</h2>
-<p>Movement - WASD or Arrow Keys</p>
-<p>Aim - Mouse</p>
-<p>Attack - Left-Click</p>
-<p>Change Weapons - 1 through 3 or Scroll Wheel</p>
-<p>Pickup/Loot/Interact - F</p>
-<p>Reload - R</p>
-<p>Use Healing Item - Left-Click on item or 7 through 0</p>
-<p>Cancel Action - X</p>
-<p>Toggle Fullscreen Map - G or M</p>
-<p>Toggle Minimap - N</p>
-<p>Use Emote Wheel - Hold Right-Click, drag, and release</p>
-<p>Drop Item - Right-Click on item</p>
-<p>Equip Last Item - Q</p>
-<p>Swap Gun Slots - T</p>
-<p>Equip Other Gun - Space</p>
-<p>Change Scope Zoom - Left-Click on Scope</p>
+<p>Movimento - WASD ou teclas de seta</p>
+<p>Mirar - Mouse</p>
+<p>Ataque - Clique esquerdo</p>
+<p>Alterar armas - 1 a 3 ou roda de rolagem</p>
+<p>Capturar/Saquear/Interagir - F</p>
+<p>Recarregar arma - R</p>
+<p>Use Item de Cura - Clique com o botão esquerdo no item ou 7 a 0</p>
+<p>Cancelar ação - X</p>
+<p>Alternar mapa em tela cheia - G ou M</p>
+<p>Alternar Minimapa - N</p>
+<p>Use o emogi de gestos - segure, clique com o botão direito, arraste e solte</p>
+<p>Soltar Item - Clique com o botão direito no item</p>
+<p>Equipar Último Item - Q</p>
+<p>Trocar slots de armas - T</p>
+<p>Equipar outra arma - Espaço</p>
+<p>Alterar zoom do escopo - clique com o botão esquerdo no escopo</p>
 
-<h3>Keybinds can be changed in settings!</h3>
+<h3>Os atalhos de teclado podem ser alterados nas configurações!</h3>
 
-<h2>How to Play</h2>
-<p>The goal of Suroi is to be the last player standing. You'll start with nothing, so move around the map to get weapons, scopes, armor, healing items, and ammo. If you see another player, eliminate them for their loot!</p>
+<h2>Como jogar</h2>
+<p>O objetivo de Suroi é ser o último jogador sobrevivente. Você começará do nada, então mova-se pelo mapa para obter armas, miras, armaduras, itens de cura e munição. Se você vir outro jogador, elimine-o para saquear!</p>
 
-<h2>Orange = Bad</h2>
-<p>You have another enemy to watch out for: a deadly orange gas. The gas slowly fills the map as the game progresses, making the safe zone smaller and smaller. The damage increases each time it shrinks.</p>
+<h2>Laranja = Ruim</h2>
+<p>Você tem outro inimigo para ficar atento: um gás laranja mortal. O gás preenche lentamente o mapa conforme o jogo avança, tornando a zona segura cada vez menor. O dano aumenta cada vez que diminui.</p>
 
-<h2>Similar Games</h2>
-<p>If you've played games like PUBG, Fortnite, and Apex Legends, or other browser battle royales like surviv.io and zombsroyale.io, you might be familiar with the concept of the game!</p>
+<h2>Jogos semelhantes</h2>
+<p>Se você já jogou jogos como PUBG, Fortnite e Apex Legends, ou outros battle royales de navegador, talvez esteja familiarizado com o conceito do jogo!</p>
 `
     }
 ];

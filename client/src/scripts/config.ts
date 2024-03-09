dd02b95d69733d5090f4e72d53b00df29607ebd8
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-03-09 11:57:44 -0300

    Update config.ts

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
     regions: {
-        dev: { name: "Batalha Solo", address: "146.19.9.234:22", https: false },
+        dev: { name: "Batalha Solo", address: "146.19.9.234", https: false },
     },
     defaultRegion: "dev",
     mode: "normal"

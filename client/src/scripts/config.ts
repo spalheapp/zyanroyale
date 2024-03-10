60dc1939ccfeb4046a6408dc3e0f2840d757b203
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-03-10 11:54:25 -0300

    Update config.ts

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,7 +1,9 @@
 export const Config = {
   regions: {
     dev: { name: "Mata Mata", address: "zyanroyale.online", https: true },
-    br: { name: "Campeonato", address: "zyanroyale.online:443", https: false },
+    kj: { name: "Local Server", address: "127.0.0.1:63333", https: false },
+    sg: { name: "Singapore", address: "suroi15.dafox.box.ca", https: true },
+    nj: { name: "New Jersey", address: "islandrain.compositr.dev", https: true }
   },
   defaultRegion: "dev",
   mode: "normal",

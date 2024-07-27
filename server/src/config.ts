6ba8b0ac652665de2fbae14e4fad5a0b211bdbeb
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-07-27 15:41:38 -0300

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -34,7 +34,7 @@ export const Config = {
 
     modes: [
         { mapName: "main", teamMode: TeamMode.Solo, enabled: true },
-        { mapName: "main", teamMode: TeamMode.Duo, enabled: true },
+        { mapName: "desert", teamMode: TeamMode.Duo, enabled: true },
         { mapName: "main", teamMode: TeamMode.Squad, enabled: true }
     ],
 

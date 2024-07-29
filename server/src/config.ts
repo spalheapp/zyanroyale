70f41cab1d9d3506d505bddba75773c2e655dca1
Marcos <71572572+spalheapp@users.noreply.github.com>
2024-07-29 10:02:49 -0300

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -33,7 +33,7 @@ export const Config = {
     apiKey: "Kongregate Sucks",
 
     modes: [
-        { mapName: "desert", teamMode: TeamMode.Solo, enabled: true },
+        { mapName: "main", teamMode: TeamMode.Solo, enabled: true },
         { mapName: "main", teamMode: TeamMode.Duo, enabled: true },
         { mapName: "main", teamMode: TeamMode.Squad, enabled: true }
     ],
